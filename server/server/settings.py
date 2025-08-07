ea248fc8449254e7f46ec762ece24ca2db059684
Sashi <sashi1174@gmail.com>
2025-08-07 17:59:03 +0530

    changes made

--- server/server/settings.py
+++ server/server/settings.py
@@ -106,14 +106,23 @@ WSGI_APPLICATION = "server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'RecipeRecommend',
+        'USER': 'postgres',
+        'PASSWORD': 'Sashi1174!!!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
